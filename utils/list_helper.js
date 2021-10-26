b9ff1a5d9eec69bea2edcb5ba85776381395aa41
Latrell <lll4xy@virginia.edu>
2021-10-26 18:00:41 -0400

    exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -16,7 +16,17 @@ const totalLikes = (blogs) => {
     : likesArray.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(a => a.likes)
+  const maxValue = Math.max(...likes)
+  const favorite = blogs.find(blog => blog.likes === maxValue)
+  return blogs.length === 0
+    ? 0
+    : favorite
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
